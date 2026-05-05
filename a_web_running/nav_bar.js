46745f2899b6b15e213e95882a3f4b41eeb34871
Osnia1217 <far11218858@gmail.com>
2026-05-05 11:45:01 +0800

    Add files via upload

---
+++ a_web_running/nav_bar.js
@@ -0,0 +1,18 @@
+
+    window.onload = function(){
+        document.getElementById("nav_bar").innerHTML=`<nav>
+            <div class="container">
+            <a href="index.html" class="logo">吃喝玩樂營</a>
+            <ul>
+                <li><a href="index.html" class="active">首頁</a></li>
+                <li><a href="about.html">美食美景</a></li>
+                <li><a href="courses.html">旅程簡介</a></li>
+                <li><a href="tools.html">導遊介紹</a></li>
+                <li><a href="testimonials.html">旅遊見證</a></li>
+            </ul>
+        </div>
+    </nav>;`
+        }
+        
+        
+        
